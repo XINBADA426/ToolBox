196b52cefd17346665467f650332b1633cd97457
ChaoboRen <616896512@qq.com>
2020-04-17 15:30:15 +0800

    feat(formattable.py): add a table for pretty table output

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Scripts to deal with fq/fq.gz files
 
 
 ## fa
+
+
+## table
+Scripts to deal with tables
+
+### formattable.py
+以较为美观的方式打印表格
+
---
+++ table/formattable.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: MingJia
+# @Date:   2020-04-17 14:15:17
+# @Last Modified by:   MingJia
+# @Last Modified time: 2020-04-17 14:15:17
+import logging
+
+import click
+from prettytable import PrettyTable
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+__version__ = '1.0.0'
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version=__version__)
+@click.argument('input',
+                type=click.File('r'))
+@click.option('-s', '--sep',
+              default='\t',
+              # show_default=True,
+              help="The separator of input content [default: \\t]")
+def cli(input, sep):
+    """
+    Print the input table in a pretty format
+
+    It's juest uesd for show small piece of content,
+    the max row number is 1000
+
+    """
+    res = PrettyTable()
+    i = 0
+    while True and i <= 1000:
+        line = input.readline()
+        if line:
+            arr = line.strip().split(sep)
+            if i == 0:
+                res.field_names = arr
+            else:
+                res.add_row(arr)
+        i += 1
+    res.align = 'l'
+    print(res)
+
+
+if __name__ == "__main__":
+    cli()
